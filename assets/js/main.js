1c97446bf12b125bef67b33156a330999f6551a4
ecemdiler <ecemdiler.gmail.com>
2025-04-24 16:12:06 +0300

    ilk commit

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+/*=============Reusable css classes=========*/
+/*=============Reusable css classes=========*/
+/*=============Reusable css classes=========*/
+/*=============Reusable css classes=========*/
+/*=============Reusable css classes=========*/
+/*=============Reusable css classes=========*/
+/*=============Reusable css classes=========*/
+/*=============Reusable css classes=========*/
+
+
+
+/*=============Swiper categories=========*/
+
+var swiperCategories = new Swiper(".categories__container", {
+  spaceBetween: 24,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1400: {
+      slidesPerView: 6,
+      spaceBetween: 24,
+    },
+  },
+});
+
+
+
+{/* <script>
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    }
+  });
+</script> */}
+
+
+/*=============Reusable css classes=========*/
+
+
+
+/*=============Reusable css classes=========*/

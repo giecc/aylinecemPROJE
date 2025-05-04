b460e4ceb72e451a588ef68ed552a39b4668a9d7
ecemdiler <ecemdiler.gmail.com>
2025-05-04 21:27:29 +0300

    4.05

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,70 +1,34 @@
 /*=============Reusable css classes=========*/
-/*=============Reusable css classes=========*/
-/*=============Reusable css classes=========*/
-/*=============Reusable css classes=========*/
-/*=============Reusable css classes=========*/
-/*=============Reusable css classes=========*/
-/*=============Reusable css classes=========*/
-/*=============Reusable css classes=========*/
+
+/*=============image galery=========*/
+
+function imgGallery() {
+  const mainImg = document.querySelector('.details__img'),
+    smallImg = document.querySelectorAll('.details__small-img');
+
+
+  smallImg.forEach((img) => {
+    img.addEventListener('click', function () {
+      mainImg.src = this.src;
+    })
+  })
+
+}
+
+imgGallery();
 
 
 
-/*=============Swiper categories=========*/
-
-// var swiperCategories = new Swiper(".categories__container", {
-//   spaceBetween: 20,
-//   loop: true,
-
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   breakpoints: {
-//     640: {
-//       slidesPerView: 2,
-//       spaceBetween: 20,
-//     },
-//     768: {
-//       slidesPerView: 4,
-//       spaceBetween: 40,
-//     },
-//     1400: {
-//       slidesPerView: 6,
-//       spaceBetween: 24,
-//     },
-//   },
-// });
 
 
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   var swiperCategories = new Swiper(".categories__container", {
-//     spaceBetween: 20,
-//     loop: true,
-//     slidesPerView: 'auto', // Otomatik genişlik ayarı
-//     centeredSlides: true, // Slaytları ortalar
 
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//     breakpoints: {
-//       320: {
-//         slidesPerView: 2,
-//         centeredSlides: false
-//       },
-//       640: {
-//         slidesPerView: 3
-//       },
-//       768: {
-//         slidesPerView: 4
-//       },
-//       1024: {
-//         slidesPerView: 5
-//       }
-//     }
-//   });
-// });
+
+
+
+
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
   var swiper = new Swiper('.categories__container', {
@@ -93,13 +57,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
 /*=============PRODUCTS TABS=========*/
 const tabs = document.querySelectorAll('[data-target]'),
-  tabContents = document.querySelectorAll('[content]');
+  tabContents = document.querySelectorAll('[data-content]');
 
 
 tabs.forEach((tab) => {
   tab.addEventListener('click', () => {
     const target = document.querySelector(tab.dataset.target);
     console.log(target);
+
     tabContents.forEach((tabContent) => {
       tabContent.classList.remove('active-tab');
     });
@@ -115,4 +80,59 @@ tabs.forEach((tab) => {
     tab.classList.add('active-tab');
 
   });
-}); 
+});
+
+
+
+
+/*=============shop.html=========*/
+
+// Shop menüsünü aç/kapat
+// document.getElementById('shop-menu').addEventListener('mouseenter', function () {
+//   document.getElementById('main-dropdown').classList.add('show');
+// });
+
+// document.getElementById('shop-menu').addEventListener('mouseleave', function () {
+//   document.getElementById('main-dropdown').classList.remove('show');
+// });
+
+// // Alt menü seçeneklerine tıklama
+// const submenuTriggers = document.querySelectorAll('.submenu-trigger');
+
+// submenuTriggers.forEach(trigger => {
+//   trigger.addEventListener('click', function (e) {
+//     e.preventDefault();
+
+//     // Tüm içerikleri gizle
+//     document.querySelectorAll('.category-content').forEach(content => {
+//       content.classList.remove('active');
+//     });
+
+//     // Tıklanan kategoriyi göster
+//     const category = this.getAttribute('data-category');
+//     document.getElementById(`${category}-content`).classList.add('active');
+
+//     // Menüyü kapat
+//     document.getElementById('main-dropdown').classList.remove('show');
+//   });
+// });
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const shopMenu = document.querySelector('shop-menu');
+  const dropdown = document.querySelector('shop-dropdown');
+
+  shopMenu.addEventListener('mouseenter', function () {
+    dropdown.style.display = 'block';
+    dropdown.style.opacity = '1';
+    dropdown.style.visibility = 'visible';
+  });
+
+  shopMenu.addEventListener('mouseleave', function () {
+    dropdown.style.display = 'none';
+    dropdown.style.opacity = '0';
+    dropdown.style.visibility = 'hidden';
+  });
+});
